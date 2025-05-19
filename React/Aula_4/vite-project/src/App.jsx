738d9ed41f78bce81f7691420a8a21243ad36498
Kaua676 <kauavic676@gmail.com>
2025-05-19 17:01:06 -0400

    feat: exercicios react

---
+++ React/Aula_4/vite-project/src/App.jsx
@@ -0,0 +1,32 @@
+import Exercicio1 from "./exercicio1/Exercicio1.jsx";
+import Exercicio2 from "./exercicio2/Exercicio2.jsx";
+import Exercicio3 from "./exercicio3/Exercicio3.jsx";
+import Exercicio4 from "./exercicio4/Exercicio4.jsx";
+import Exercicio5 from "./exercicio5/Exercicio5.jsx";
+import Header from "./exemplos/Header.jsx";
+import Footer from "./exemplos/Footer.jsx";
+
+function App() {
+  return (
+    <>
+      <Exercicio1 />
+      <hr />
+      <Exercicio2 />
+      <hr />
+      <Exercicio3 />
+      <hr />
+
+      <Exercicio4 />
+      <hr />
+
+      <Exercicio5 />
+      <hr />
+
+      <Header />
+      <p>Esse seria o conteúdo</p>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ React/Aula_4/vite-project/src/exercicio1/Exercicio1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OlaReact = () => {
+  return <h1>Olá React!</h1>;
+};
+
+const App = () => {
+  return <OlaReact />;
+};
+
+export default App;
